55c44a726c5a7f33f945891c4a529e2b754a7d84
Jeff <jeff.galbraith@semantic-network.com>
2023-03-11 13:33:16 -0700

    fix: center tooltip correctly relative to the data

--- src/components/Account.vue
+++ src/components/Account.vue
@@ -2,7 +2,7 @@
   <div v-if="accountId">
     <div v-if="accountId === 'fhCBCt9jfV4TJ2c4P8fymVHgTE7oXtQhNHcg5AShpF4x1S4Gr'"><q-badge color="green-8" text-color="white">All Time High</q-badge></div>
     <div v-else-if="accountId === 'fhCBCt9jfV4TJ2dvdhAkca1FQqxbEjyyXe7SczZ9V8TLuK7pb'"><q-badge color="orange-8" text-color="white">Sunrise Rewards</q-badge></div>
-    <div v-else-if="selectedAccount && accountId === selectedAccount"><q-badge>This Account</q-badge><q-tooltip>{{ accountId }}</q-tooltip></div>
+    <div v-else-if="selectedAccount && accountId === selectedAccount"><q-badge>This Account<q-tooltip>{{ accountId }}</q-tooltip></q-badge></div>
     <div v-else>
       <Identicon :address="accountId" />
       <router-link
