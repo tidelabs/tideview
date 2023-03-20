bc86a39a5b84bef48b7900817d9460867e855a59
Jeff <jeff.galbraith@semantic-network.com>
2023-03-20 06:37:44 -0600

    fix: use correct ATH account

--- src/components/AccountId.vue
+++ src/components/AccountId.vue
@@ -1,8 +1,8 @@
 <template>
   <div v-if="accountId">
     <div v-if="accountId === 'fhFNDGKtRFfPJZbJZMNLRNwTC6rDJJt3RofUrz7vhMqbrDcBf'"><q-badge color="green-8" text-color="white">Market Maker</q-badge></div>
-    <!-- <div v-else-if="accountId === 'fhCBCt9jfV4TJ2c4P8fymVHgTE7oXtQhNHcg5AShpF4x1S4Gr'"><q-badge color="blue-8" text-color="white">All Time High</q-badge></div> -->
-    <div v-else-if="accountId === 'fhCMUeg6DisVdRfbK29U1TFpSiWJHceZsPGwpQzGoyy1tJ1Q8'"><q-badge color="blue-8" text-color="white">All Time High</q-badge></div>
+    <div v-else-if="accountId === 'fhCBCt9jfV4TJ2c4P8fymVHgTE7oXtQhNHcg5AShpF4x1S4Gr'"><q-badge color="yellow-8" text-color="black">All Time High</q-badge></div>
+    <!-- <div v-else-if="accountId === 'fhCMUeg6DisVdRfbK29U1TFpSiWJHceZsPGwpQzGoyy1tJ1Q8'"><q-badge color="blue-8" text-color="white">All Time High</q-badge></div> -->
     <div v-else-if="accountId === 'fhCBCt9jfV4TJ2dvdhAkca1FQqxbEjyyXe7SczZ9V8TLuK7pb'"><q-badge color="orange-8" text-color="white">Sunrise Rewards</q-badge></div>
     <div v-else-if="selectedAccount && accountId === selectedAccount"><q-badge>This Account<q-tooltip>{{ accountId }}</q-tooltip></q-badge></div>
     <div v-else>
