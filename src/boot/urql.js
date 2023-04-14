a3e48226c666970368842da6cb844d33678950a7
Jeff <jeff.galbraith@semantic-network.com>
2023-04-14 13:52:57 -0600

    fix: urql fixes for latest version

--- src/boot/urql.js
+++ src/boot/urql.js
@@ -1,10 +1,12 @@
 import { boot } from 'quasar/wrappers'
 import urql from '@urql/vue'
+import { cacheExchange, fetchExchange } from '@urql/core'
 
 // "async" is optional;
 // more info on params: https://v2.quasar.dev/quasar-cli/boot-files
 export default boot(async ({ app }) => {
   app.use(urql, {
-    url: 'https://squid.subsquid.io/tidechain-tidescan/v/v2/graphql'
+    url: 'https://squid.subsquid.io/tidechain-tidescan/v/v2/graphql',
+    exchanges: [ cacheExchange, fetchExchange ]
   })
 })
